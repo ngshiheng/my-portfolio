e14fa579ce431c8a7ba66236f48ca923b3d58884
ngshiheng <ngshiheng@gmail.com>
2019-05-01 23:51:02 +0800

    Updated page content using Django template language. Added some projects into database

--- my_portfolio/portfolio/views.py
+++ my_portfolio/portfolio/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Project
 
 
 def profile(request):
     return render(request, 'portfolio/index.html', {'title': 'Jerry\'s Portfolio'})
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'portfolio/index.html'
+    context_object_name = 'projects'
+
+
