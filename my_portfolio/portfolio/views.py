31c44cde3d9b1d8afa5086682e828311c091649b
ngshiheng <ngshiheng@gmail.com>
2019-05-19 15:50:29 +0800

    Added contact me form

--- my_portfolio/portfolio/views.py
+++ my_portfolio/portfolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, FormView
+from django.views.generic import FormView
 from .models import Project
 from .forms import ContactForm
 from django.core.mail import send_mail
@@ -10,31 +10,33 @@ def profile(request):
     return render(request, 'portfolio/index.html', {'title': 'Jerry\'s Portfolio'})
 
 
-class MainView(ListView, FormView):
-    model = Project
-    context_object_name = 'projects'
+# contacts view for the main page with all the portfolios as well as a 'Contact Me' form.
+class MainView(FormView):
     template_name = 'portfolio/index.html'
     form_class = ContactForm
     success_url = '/#contact'
 
     # to populate a dictionary to use as the template context from the model Project
-    def get_context_data(self, *, object_list=None, **kwargs):
-        data = super().get_context_data(**kwargs)
-        data['title'] = 'Project'
-        return data
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['project_list'] = Project.objects.all()
+        return context
 
     # check if the form filled at Contact Me is valid. If yes, proceed to send an email
     def form_valid(self, form):
-        message = "NAME -- {name}\nEMAIL -- {email}\nMOBILE -- {mobile}\n ".format(
+        subject = "{name} contacted you through your Django Portfolio page".format(
+            name=form.cleaned_data.get('name')
+        )
+        message = "Hey there! I am {name}, you can reach me through my email {email} or call me at {mobile}.".format(
             name=form.cleaned_data.get('name'),
             email=form.cleaned_data.get('email'),
             mobile=form.cleaned_data.get('mobile'))
 
         message += "\n\n{0}".format(form.cleaned_data.get('message'))
         send_mail(
-            subject='Someone contacted you through your Django Portfolio',
+            subject=subject,
             message=message,
             from_email=form.cleaned_data.get('email'),
-            recipient_list=[settings.EMAIL_HOST_USER],      # email to myself
+            recipient_list=[settings.EMAIL_HOST_USER],  # email to myself
         )
         return super(MainView, self).form_valid(form)
